a69be998a3fcc3deab6312d37c1160d9864464b4
Kelsey A <d.chan42@gmail.com>
2024-02-20 12:12:45 -0500

    adding display a selection list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,12 +1,19 @@
+import {
+  /* . . . */
+  NgFor, NgIf,
+  /* . . . */
+} from '@angular/common';
+
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import {HEROES} from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, NgFor,NgIf,],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css',
 })
@@ -15,5 +22,11 @@ export class HeroesComponent {
     id: 1,
     name: 'Windstorm'
   };
+  heroes = HEROES;
+
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
 }
